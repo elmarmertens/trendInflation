37910b203021eb49a9c3525373cd35c67f226d40
Elmar Mertens <em@elmarmertens.com>
2021-10-27 21:00:29 +0200

    added SDW-SPF data
    Signed-off-by: Elmar Mertens <em@elmarmertens.com>

--- getSDW.m
+++ getSDW.m
@@ -87,6 +87,10 @@ datalabel = 'SDWINF';
 Ydata  = [Ydata, GDPD];
 Ylabel = cat(2, Ylabel, 'GDPD');
 
+% save INF for later
+Yinf     = Ydata;
+INFlabel = Ylabel;
+
 Ny             = size(Ydata, 2);
 yNaNndx        = isnan(Ydata);
 Ydata(yNaNndx) = 0;
@@ -192,3 +196,107 @@ display(filename);
 type(filename)
 hrulefill
 
+%% add SPF
+SPFimport    = importdata('SDWINFSRV.csv');
+spfdates     = datenum(SPFimport.textdata(6:end,1), 'yyyy-mm-dd');
+spfdates     = flipud(spfdates);
+
+% grab only longer-term SPF data
+% col 1: HICP
+% Col 2-4: 12m,24m,LTR
+Nspf     = 3;
+spfdata  = SPFimport.data(:,2:4);
+spfdata  = flipud(spfdata);
+spfdata  = log(1 + spfdata / 100) * 100;
+
+Yspf     = NaN(T,Nspf); % target array 
+
+for t = 1 : length(spfdates)
+    
+    % 12m: SDW stores survey response at target data
+    i = 1;
+    [y,m]        = datevec(spfdates(t));
+    thisdate     = datenum(y-1,m,1); % always dated at first of month
+    ndx          = dates == thisdate;
+    Yspf(ndx, i) = spfdata(t,i);
+    
+    % 24m: SDW stores survey response at target data
+    i = 2;
+    [y,m]        = datevec(spfdates(t));
+    thisdate     = datenum(y-2,m,1); % always dated at first of month
+    ndx          = dates == thisdate;
+    Yspf(ndx, i) = spfdata(t,i);
+    
+    % LTR: SDW stores survey response at last day of quarter SPF was conducted
+    i = 3;
+    % the 
+    if ~isnan(spfdata(t,i))
+        [y,m] = datevec(spfdates(t));
+        % Figure out last month prior to survey quarter
+        if m > 3
+            thisdate = datenum(y,m-3,1); 
+        elseif m == 3
+            thisdate = datenum(y-1,12,1);
+        else
+            error('m lower than three not expected') % LTR data enterd for months 3,6,9 and 12
+        end
+        ndx              = dates == thisdate;
+        Yspf(ndx, i) = spfdata(t,i);
+    end
+    
+end
+
+SPFlabel = {'SPF-12m', 'SPF-24m', 'SPF-LTR'};
+figure
+hold on
+plot(dates, Yspf, '-x')
+xtickdates(dates)
+title('SPF')
+
+datalabel = 'SDWINFTRMSRV';
+Ydata     = [Ydata, Yspf];
+Ylabel    = cat(2, Ylabel, SPFlabel);
+Ny             = size(Ydata, 2);
+yNaNndx        = isnan(Ydata);
+Ydata(yNaNndx) = 0;
+
+mat2fortran(sprintf('%s.yData.txt', datalabel), Ydata)
+logical2fortran(sprintf('%s.yNaN.txt', datalabel), yNaNndx)
+mat2fortran(sprintf('%s.dates.txt', datalabel), dates)
+
+filename = sprintf('%s.settings.txt', datalabel);
+fid = fopen(filename, 'wt');
+fprintf(fid, 'Ny = %d\n', size(Ydata,2));
+fprintf(fid, 'T  = %d\n', size(Ydata,1));
+fprintf(fid, 'YLABEL:\n');
+for n = 1 : Ny
+    fprintf(fid, '%s\n', Ylabel{n});
+end
+fclose(fid);
+display(filename);
+type(filename)
+hrulefill
+
+datalabel = 'SDWINFSRV';
+Ydata     = [Yinf, Yspf];
+Ylabel    = cat(2, INFlabel, SPFlabel);
+Ny             = size(Ydata, 2);
+yNaNndx        = isnan(Ydata);
+Ydata(yNaNndx) = 0;
+
+mat2fortran(sprintf('%s.yData.txt', datalabel), Ydata)
+logical2fortran(sprintf('%s.yNaN.txt', datalabel), yNaNndx)
+mat2fortran(sprintf('%s.dates.txt', datalabel), dates)
+
+filename = sprintf('%s.settings.txt', datalabel);
+fid = fopen(filename, 'wt');
+fprintf(fid, 'Ny = %d\n', size(Ydata,2));
+fprintf(fid, 'T  = %d\n', size(Ydata,1));
+fprintf(fid, 'YLABEL:\n');
+for n = 1 : Ny
+    fprintf(fid, '%s\n', Ylabel{n});
+end
+fclose(fid);
+display(filename);
+type(filename)
+hrulefill
