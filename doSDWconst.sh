486ef776eae08e50677988aacfdc01a455481f0e
Elmar Mertens <em@elmarmertens.com>
2022-04-05 16:49:18 +0200

    new: doSDW(const)

---
+++ doSDWconst.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source /opt/intel/oneapi/setvars.sh
+
+export OMP_NUM_THREADS=4
+
+this=mcmcPaddington
+Tdata=0
+
+for datalabel in SDWINF SDWINFTRM INFTRMSRV
+do
+    echo $datalabel
+    make THIS=$this run DATALABEL=$datalabel
+done
+
